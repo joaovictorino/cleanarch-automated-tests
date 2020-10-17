32736fbc91934a2227d58ea19a4596b1548265f1
p017395 <jh_vs@hotmail.com>
2020-10-17 00:18:22 -0300

    initial version

---
+++ bank/src/main/java/com/bank/account/model/Account.java
@@ -0,0 +1,5 @@
+package com.bank.account.model;
+
+public class Account 
+{
+}
---
+++ bank/src/test/java/com/bank/account/AccountTest.java
@@ -0,0 +1,15 @@
+package com.bank.account;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+import com.bank.account.model.Account;
+
+public class AccountTest 
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertEquals( true, true );
+    }
+}
