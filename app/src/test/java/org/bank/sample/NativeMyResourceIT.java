01d5db1935c35565c6d81fc314ac8cb8856d696f
p017395 <jh_vs@hotmail.com>
2020-10-16 23:00:36 -0300

    cleaning repo

--- app/src/test/java/org/bank/sample/NativeMyResourceIT.java
+++
@@ -1,9 +0,0 @@
-package org.bank.sample;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeMyResourceIT extends MyResourceTest {
-
-    // Execute the same tests but in native mode.
-}
