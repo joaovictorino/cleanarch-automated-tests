bf9165c73a50b9b0bbf2e7902f4851568c61be1b
p017395 <jh_vs@hotmail.com>
2020-10-18 20:06:11 -0300

    quarkus/spring boot

---
+++ bankboot/src/main/java/com/bank/service/boot/bankboot/BankbootApplication.java
@@ -0,0 +1,13 @@
+package com.bank.service.boot.bankboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankbootApplication.class, args);
+	}
+
+}
---
+++ bankboot/src/test/java/com/bank/service/boot/bankboot/BankbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.service.boot.bankboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
