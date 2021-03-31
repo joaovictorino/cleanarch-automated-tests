b6b8d84957a995d1cafeb67f2eb14158bdde6886
joaovictorino <jhvictorino@gmail.com>
2021-03-31 15:21:38 -0300

    restassured sample

---
+++ restassured/src/test/java/com/bank/account/restassured/TransferControllerRestAssuredTest.java
@@ -0,0 +1,31 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.post;
+
+import io.restassured.RestAssured;
+
+public class TransferControllerRestAssuredTest {
+    
+    @BeforeAll
+    public static void setBaseURI() {
+        RestAssured.baseURI = "http://localhost:8080";
+        get("/transfer").then().statusCode(200);
+    }
+
+    @Test
+    public void transferMoneySuccess() {
+        post("/transfer/123456/654321/200").then().statusCode(200);
+        float balanceFrom = get("/account/123456").then().assertThat().statusCode(200).extract().path("balance");
+        float balanceTo = get("/account/654321").then().assertThat().statusCode(200).extract().path("balance");
+        assertEquals(4800.0, balanceFrom);
+        assertEquals(5200.0, balanceTo);
+    }
+
+    @Test
+    public void transferMoneyFailure() {
+        post("/transfer/123456/654321/-20").then().statusCode(400);
+    }
+}
